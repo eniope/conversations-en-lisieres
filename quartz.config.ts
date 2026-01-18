71c7b40019f03f9134f232b6740b6306e12b5f5b
Stéphane Caillaud <s.caillaud@gmail.com>
2026-01-18 10:23:53 +0100

    Add console log for loaded Quartz config

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,3 +1,5 @@
+console.log(">>> LOADED QUARTZ CONFIG: ASSETS STATICDIR <<<")
+
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 import { Citations } from "./quartz/plugins/transformers/citations"
